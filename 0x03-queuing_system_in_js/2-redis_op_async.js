9656cb302c273c164e0719ec5b81a8e84fdfb2bc
olakunle-remi <abbeypentious@gmail.com>
2024-12-02 06:53:18 -0800

    0x03-queuing_system_in_js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,17 @@
+import { createClient, print } from "redis";
+import { promisify } from "util";
+
+const client = createClient();
+
+client
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
